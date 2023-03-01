947c8146aaa550cc0d96bb176500ee670e036b8e
Lian Liu <riceudon@gmail.com>
2023-02-28 21:48:41 -0800

    changed "let" to "const" in noteRoutes.js

--- controllers/api/noteRoutes.js
+++ controllers/api/noteRoutes.js
@@ -7,8 +7,8 @@ const dbPath = path.join(__dirname, '../../db/db.json')
 // GET /api/notes
 router.get('/notes', async (req, res) => {
   try {
-    let data = await fs.promises.readFile(dbPath);
-    let notes = JSON.parse(data);
+    const data = await fs.promises.readFile(dbPath);
+    const notes = JSON.parse(data);
     res.json(notes);
   } catch (err) {
     throw err
@@ -18,9 +18,9 @@ router.get('/notes', async (req, res) => {
 // POST /api/notes
 router.post('/notes', async (req, res) => {
   try {
-    let data = await fs.promises.readFile(dbPath);
-    let notes = JSON.parse(data);
-    let newNote = {
+    const data = await fs.promises.readFile(dbPath);
+    const notes = JSON.parse(data);
+    const newNote = {
       id: (notes.length + 1),
       title: req.body.title,
       text: req.body.text
@@ -37,13 +37,13 @@ router.post('/notes', async (req, res) => {
 // DELETE /api/notes/:id
 router.delete('/notes/:id', async (req, res) => {
   try {
-    let data = await fs.promises.readFile(dbPath);
-    let notes = JSON.parse(data);
-    let deletedNote = notes.find(note => note.id === Number(req.params.id));
+    const data = await fs.promises.readFile(dbPath);
+    const notes = JSON.parse(data);
+    const deletedNote = notes.find(note => note.id === Number(req.params.id));
     if (!deletedNote) {
       return res.status(404).json({ error: 'Note not found' });
     }
-    let updatedNotes = notes.filter(note => note.id !== Number(req.params.id));
+    const updatedNotes = notes.filter(note => note.id !== Number(req.params.id));
     await fs.promises.writeFile(dbPath, JSON.stringify(updatedNotes));
     res.json(deletedNote);
   } catch (err) {
